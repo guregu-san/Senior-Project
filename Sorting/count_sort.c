b23a34aeb3a71d8e74330a2b24846a4657172829
Greg <206912153+guregu-san@users.noreply.github.com>
2026-04-27 19:12:06 +0300

    Added sorting algorithms

---
+++ Sorting/count_sort.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <omp.h>
+#define SIZE 10
+
+int main() {
+    int nums[SIZE], temp[SIZE];
+
+    srand((int)time(NULL));
+
+    for (int i=0; i < SIZE; i++)
+    {
+        nums[i] = rand() % SIZE;
+        temp[i] = -1;
+        printf("%d ", nums[i]);
+    }
+    printf("\n");
+
+    #pragma omp parallel default(none) shared(nums, temp)
+    {
+        #pragma omp for
+        for (int i=0; i < SIZE; i++)
+        {
+            int currentNum = nums[i], position = 0;
+
+            for (int k = 0; k < SIZE; k++)
+                if (currentNum > nums[k] || nums[k] == currentNum && i > k)
+                    position++;
+
+            printf("Thread %d doing iteration %d calculated position %d for number %d at %x\n", omp_get_thread_num(), i, position, currentNum, &currentNum);
+            temp[position] = currentNum;
+        }
+
+        #pragma omp for
+        for (int i=0; i < SIZE; i++)
+            nums[i] = temp[i];
+    }
+
+    for (int i=0; i < SIZE; i++)
+    {
+        printf("%d ", nums[i]);
+    }
+    printf("\n");
+
+    return 0;
+}
